faa5ea1a2ccd475764d3c38e443c979728c0ec27
PaulinaPacyna <pacyna1998@gmail.com>
2022-05-22 16:59:03 +0200

    functions moved to utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 import pandas as pd
 
@@ -11,3 +13,42 @@ def get_stations(columns=None):
         for place in city['places']:
             data.append({col: place[col] for col in columns})
     return pd.DataFrame(data)
+
+
+def read_bikes(path: str = './data') -> pd.DataFrame:
+    bikes = pd.concat([pd.read_csv(os.path.join(path, p)) for p in os.listdir(path)]).reset_index()
+    bikes['date'] = pd.to_datetime(bikes['date'])
+    bikes['day'] = bikes['date'].dt.floor('d')
+    return bikes
+
+
+def rented_returned_amount(df: pd.DataFrame) -> pd.DataFrame:
+    """For each station return number of bikes that were just returned and rented"""
+    df_copy= df.copy()
+    df_copy['prev_station'] = df_copy \
+        .sort_values(by=['date'], kind='stable') \
+        .groupby(['bike_number']) \
+        .shift(1)['station_id'].astype('Int64')
+    df_copy['next_station'] = df_copy \
+        .sort_values(by=['date'], kind='stable') \
+        .groupby(['bike_number']) \
+        .shift(-1)['station_id'].astype('Int64')
+    # You can't do next != current because it does not detect loops
+    df_copy['just_returned'] = df_copy \
+                              .sort_values(by=['date'], kind='stable') \
+                              .groupby(['bike_number', 'station_id']) \
+                              .shift(1)['date'].isna() | (df_copy['prev_station'] != df_copy['station_id'])
+    df_copy['just_rented'] = df_copy \
+                            .sort_values(by=['date'], kind='stable') \
+                            .groupby(['bike_number', 'station_id']) \
+                            .shift(-1)['date'].isna() | (df_copy['next_station'] != df_copy['station_id'])
+
+    grouped = df \
+        .groupby(['date', 'station_id']) \
+        .aggregate({'bike_number': len, 'just_returned': sum, 'just_rented': sum}) \
+        .reset_index() \
+        .rename(columns={'bike_number': 'bike_count'}) \
+        .merge(get_stations(), left_on='station_id', right_on='uid')
+    grouped['just_rented'] = grouped.sort_values(['date']).groupby('station_id').shift(1)['just_rented']
+    return grouped
+
